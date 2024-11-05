89387c2b89d81efaf5f33f852a1c5c8fd93491eb
AX-AMote <Alphaeus.Mote@automox.com>
2024-11-05 07:42:04 -0500

    New

--- Get-AutomoxAPIData/Modules/ObjectGraphTools/0.2.1/Build/Build-Docs.ps1
+++
@@ -1,5 +0,0 @@
-using module ..\..\ObjectGraphTools
-Get-ChildItem $PSScriptRoot\..\Source\Public\*.ps1 | ForEach-Object {
-    Write-Host $_
-    Get-MarkdownHelp $_ | Out-File -Encoding ASCII $PSScriptRoot\..\Docs\$($_.BaseName).md
-}
